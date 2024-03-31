a6f18424b34482266cec52a5426e19d50cbf6a07
zhOhh <1299410261@qq.com>
2024-03-31 14:06:32 +0800

    修复AntDesignPro去国际化后router页面显示问题

--- linksauce-frontend/config/routes.ts
+++ linksauce-frontend/config/routes.ts
@@ -1,20 +1,3 @@
-// export default [
-//   {
-//     path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
-//   { path: '/welcome', icon: 'smile', component: './Welcome' },
-//   {
-//     path: '/admin',
-//     icon: 'crown',
-//     access: 'canAdmin',
-//     routes: [
-//       { path: '/admin', redirect: '/admin/sub-page' },
-//       { path: '/admin/sub-page', component: './Admin' },
-//     ],
-//   },
-//   { icon: 'table', path: '/list', component: './TableList' },
-//   { path: '/', redirect: '/welcome' },
-//   { path: '*', layout: false, component: './404' },
-// ];
 export default [
   { name: '登录', path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
   { name: '欢迎页面', path: '/welcome', icon: 'smile', component: './Welcome' },
