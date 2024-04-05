5117c0865ed13e8ae60154b3e88cceb020f4f5d1
zhOhh <1299410261@qq.com>
2024-04-05 16:41:09 +0800

    用户主页

--- linksauce-frontend/config/routes.ts
+++ linksauce-frontend/config/routes.ts
@@ -1,13 +1,30 @@
 export default [
-  { name: '登录', path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
-  // { name: '欢迎页面', path: '/welcome', icon: 'smile', component: './Welcome' },
+  { name: '主页', path: '/', icon: 'smile', component: './Index' },
+  {
+    name: '查看接口',
+    path: '/interface_info/:id',
+    icon: 'smile',
+    component: './InterfaceInfo',
+    hideInMenu: true,
+  },
+  {
+    name: '登录',
+    path: '/user',
+    layout: false,
+    routes: [{ path: '/user/login', component: './User/Login' }],
+  },
   {
     path: '/admin',
     icon: 'crown',
     access: 'canAdmin',
     name: '管理员页面',
     routes: [
-      { icon: 'table', path: '/admin/interface_info', component: './Admin/InterfaceInfo', name: '接口管理' },
+      {
+        icon: 'table',
+        path: '/admin/interface_info',
+        component: './Admin/InterfaceInfo',
+        name: '接口管理',
+      },
     ],
   },
 
